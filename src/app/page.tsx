640d8de6fd5514edc10a8172648ae85fb26bd6e5
Sagar Chauhan <tecorbitron@gmail.com>
2026-02-07 00:54:19 +0530

    Layout H and F

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
-import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="text-9xl">Nitara Securities</h1>
+        <main>
+            <div>
+                <h1 className="text-9xl">Nitara Securities</h1>
 
-            <nav>
-                <ul>
-                    <li>Schemes</li>
-                    <li>Rules</li>
-                    <li>Apply as Trader?</li>
-                </ul>
-            </nav>
-            <button className="bg-yellow-500 p-4 text-2xl rounded">
-                Contact
-            </button>
-        </div>
+                <nav>
+                    <ul>
+                        <li>Schemes</li>
+                        <li>Rules</li>
+                        <li>Apply as Trader?</li>
+                    </ul>
+                </nav>
+                <button className="bg-yellow-500 p-4 text-2xl rounded">
+                    Contact
+                </button>
+            </div>
+        </main>
     );
 }
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+"use client";
+function Footer() {
+    return <div>Footer</div>;
+}
+
+export default Footer;
